b87f3ef110fed7dd84bdf853a179a4b60483d409
alkpa1374 <105980104+alkpa1374@users.noreply.github.com>
2026-01-26 01:43:00 +0200

    Update MealAPIServiceTest.java

--- eclipse-workspace/workspace/MealAPI/src/test/java/testing/MealAPIServiceTest.java
+++ eclipse-workspace/workspace/MealAPI/src/test/java/testing/MealAPIServiceTest.java
@@ -23,12 +23,14 @@ import model.MealInfo;
 import service.MealAPIService;
 
 //Declare the test class for MealAPIService
-public class MealAPIServiceTest {
+public class MealAPIServiceTest 
+{
 
     //Annotation indicating that this is a test method
     @Test
     //Test method for searching a meal by exact name
-    public void testSearchMealByName() throws MealAPIException {
+    public void testSearchMealByName() throws MealAPIException 
+    {
         //Create a new instance of MealAPIService with the base API URL
         final MealAPIService mealSearchService = new MealAPIService("https://www.themealdb.com/"); 
         //Call the API to search meals by the name "Arrabiata" and store the results
@@ -36,9 +38,11 @@ public class MealAPIServiceTest {
         //Assert that the results list is not empty
         Assert.assertFalse(results.isEmpty());
         //Iterate through all meals in the results
-        for (MealInfo meal : results) {
+        for (MealInfo meal : results) 
+        {
             //Print meal details (id, name, thumbnail) to the console
-            System.out.println(
+            System.out.println
+            (
                 "MealInfo{idMeal='" + meal.getIdMeal() + "'\n, strMeal='" + meal.getStrMeal() + "'\n, strMealThumb='" + meal.getStrMealThumb() + "'\n}"
             );
         }
@@ -46,7 +50,8 @@ public class MealAPIServiceTest {
 
     //Test method for searching meals by an ingredient
     @Test
-    public void testSearchMealByIngredient() throws MealAPIException {
+    public void testSearchMealByIngredient() throws MealAPIException 
+    {
         //Initialize the MealAPIService instance
         final MealAPIService mealSearchService = new MealAPIService("https://www.themealdb.com/"); 
         //Search for meals that contain the ingredient "pasta"
@@ -63,7 +68,8 @@ public class MealAPIServiceTest {
 
     //Test searching with an invalid name or ingredient
     @Test
-    public void testSearchMealByInvalidNameOrIngredient() throws MealAPIException {
+    public void testSearchMealByInvalidNameOrIngredient() throws MealAPIException 
+    {
         //Initialize the service
         final MealAPIService mealSearchService = new MealAPIService("https://www.themealdb.com/"); 
         //Attempt to search with a name unlikely to exist
@@ -76,7 +82,8 @@ public class MealAPIServiceTest {
 
     //Test searching meals by a valid numeric ID
     @Test
-    public void testSearchMealByID() throws MealAPIException {
+    public void testSearchMealByID() throws MealAPIException 
+    {
         //Initialize service
         final MealAPIService mealSearchService = new MealAPIService("https://www.themealdb.com/"); 
         //Search for a meal using its ID
@@ -84,14 +91,16 @@ public class MealAPIServiceTest {
         //Assert that at least one result was returned
         Assert.assertFalse(results.isEmpty());
         //Print detailed information for each meal
-        for (MealInfo meal : results) {
+        for (MealInfo meal : results) 
+        {
             System.out.println("MealInfo{idMeal='" + meal.getIdMeal() + "', strMeal='" + meal.getStrMeal() + "', strMealAlternate='" + meal.getStrMealAlternate() + "', strCategory='" + meal.getStrCategory() + "', strArea='" + meal.getStrArea() + "', strInstructions='" + meal.getStrInstructions() + "', strMealThumb='" + meal.getStrMealThumb() + "', strTags='" + meal.getStrTags() + "', strYoutube='" + meal.getStrYoutube() + "', strIngredient1='" + meal.getStrIngredient1() + "', strIngredient2='" + meal.getStrIngredient2() + "', strIngredient3='" + meal.getStrIngredient3() + "', strIngredient4='" + meal.getStrIngredient4() + "', strIngredient5='" + meal.getStrIngredient5() + "', strIngredient6='" + meal.getStrIngredient6() + "', strIngredient7='" + meal.getStrIngredient7() + "', strIngredient8='" + meal.getStrIngredient8() + "', strIngredient9='" + meal.getStrIngredient9() + "', strIngredient10='" + meal.getStrIngredient10() + "', strIngredient11='" + meal.getStrIngredient11() + "', strIngredient12='" + meal.getStrIngredient12() + "', strIngredient13='" + meal.getStrIngredient13() + "', strIngredient14='" + meal.getStrIngredient14() + "', strIngredient15='" + meal.getStrIngredient15() + "', strIngredient16='" + meal.getStrIngredient16() + "', strIngredient17='" + meal.getStrIngredient17() + "', strIngredient18='" + meal.getStrIngredient18() + "', strIngredient19='" + meal.getStrIngredient19() + "', strIngredient20='" + meal.getStrIngredient20() + "', strMeasure1='" + meal.getStrMeasure1() + "', strMeasure2='" + meal.getStrMeasure2() + "', strMeasure3='" + meal.getStrMeasure3() + "', strMeasure4='" + meal.getStrMeasure4() + "', strMeasure5='" + meal.getStrMeasure5() + "', strMeasure6='" + meal.getStrMeasure6() + "', strMeasure7='" + meal.getStrMeasure7() + "', strMeasure8='" + meal.getStrMeasure8() + "', strMeasure9='" + meal.getStrMeasure9() + "', strMeasure10='" + meal.getStrMeasure10() + "', strMeasure11='" + meal.getStrMeasure11() + "', strMeasure12='" + meal.getStrMeasure12() + "', strMeasure13='" + meal.getStrMeasure13() + "', strMeasure14='" + meal.getStrMeasure14() + "', strMeasure15='" + meal.getStrMeasure15() + "', strMeasure16='" + meal.getStrMeasure16() + "', strMeasure17='" + meal.getStrMeasure17() + "', strMeasure18='" + meal.getStrMeasure18() + "', strMeasure19='" + meal.getStrMeasure19() + "', strMeasure20='" + meal.getStrMeasure20() + "', strSource='" + meal.getStrSource() + "', strImageSource='" + meal.getStrImageSource() + "', strCreativeCommonsConfirmed='" + meal.getStrCreativeCommonsConfirmed() + "', dateModified='" + meal.getDateModified() + "'}");
         }
     }
 
     //Test searching meals with an invalid numeric ID
     @Test
-    public void testSearchMealByInvalidNumericID() throws MealAPIException {
+    public void testSearchMealByInvalidNumericID() throws MealAPIException 
+    {
         //Initialize service
         final MealAPIService mealSearchService = new MealAPIService("https://www.themealdb.com/"); 
         //Attempt to fetch a meal using a non-existent numeric ID
@@ -104,7 +113,8 @@ public class MealAPIServiceTest {
 
     //Test searching meals with an invalid string ID
     @Test
-    public void testSearchMealByInvalidStringID() throws MealAPIException {
+    public void testSearchMealByInvalidStringID() throws MealAPIException 
+    {
         //Initialize service
         final MealAPIService mealSearchService = new MealAPIService("https://www.themealdb.com/"); 
         //Attempt to fetch a meal using an invalid string ID
@@ -117,7 +127,8 @@ public class MealAPIServiceTest {
 
     //Test retrieving a random meal
     @Test
-    public void testSearchMealRandom() throws MealAPIException {
+    public void testSearchMealRandom() throws MealAPIException 
+    {
         //Initialize service
         final MealAPIService mealSearchService = new MealAPIService("https://www.themealdb.com/"); 
         //Call API to get a random meal
