286093db69e04326d0d95b233a115569b28b1e80
alkpa1374 <105980104+alkpa1374@users.noreply.github.com>
2026-01-26 21:48:31 +0200

    Update MealResult.java

--- eclipse-workspace/workspace/MealAPI/src/main/java/model/themealdb/MealResult.java
+++ eclipse-workspace/workspace/MealAPI/src/main/java/model/themealdb/MealResult.java
@@ -1,38 +1,37 @@
-//Defines the package in which this class resides
-//This allows other classes in the same package to access it and organizes the code logically
+//Class «MealResult» belongs to package «model.themealdb»
 package model.themealdb;
 
-//Imports the LinkedHashMap class for storing key-value pairs with insertion order preserved
+//Imports the LinkedHashMap class for storing key-value pairs in insertion order
 import java.util.LinkedHashMap;
 
 //Imports the List interface to store collections of Meal objects in an ordered sequence
 import java.util.List;
 
-//Imports the Map interface for storing key-value pairs
+//Imports the generic Map interface for key-value pair mappings
 import java.util.Map;
 
-//Imports Jackson annotation to indicate a getter method for additional JSON properties
+//Imports Jackson annotation to include additional JSON properties dynamically during serialization
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 
-//Imports Jackson annotation to indicate a setter method for additional JSON properties
+//Imports Jackson annotation to set additional JSON properties dynamically during deserialization
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 
-//Imports Jackson annotation to mark fields to be ignored during serialization/deserialization
+//Imports Jackson annotation to ignore fields during JSON serialization/deserialization
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-//Imports Jackson annotation to include only non-null fields when serializing to JSON
+//Imports Jackson annotation to control whether null fields are included in JSON
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-//Imports Jackson annotation to map a Java field to a JSON property
+//Imports Jackson annotation to explicitly map JSON properties to Java fields
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-//Imports Jackson annotation to define the order of JSON properties when serializing
+//Imports Jackson annotation to specify the order of JSON properties when serializing
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-//Ensures that only non-null fields will be included when serializing this object to JSON
+//Ensures that only fields that are not null are included when serializing this object to JSON
 @JsonInclude(JsonInclude.Include.NON_NULL)
 
-//Specifies the order in which fields should appear when serialized to JSON
+//Specification of the order of properties during serialization to JSON
 @JsonPropertyOrder
 (
   {
@@ -43,23 +42,23 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
   }
 )
 
-//Declares the MealResult class, which maps the API response for a list of meals
+//Class «MealResult» for storage data from REST API to meals list on pages
 public class MealResult 
 {
 
-    //Maps the "page" property from JSON to this Java field
+    //Mapping of the "page" JSON property to this field, storing pages
     @JsonProperty("page")
     private Integer page;
     
-    //Maps the "results" property from JSON to this Java field, which is a list of Meal objects
+    //Mapping of the "results" JSON property to this field, storing meals
     @JsonProperty("results")
     private List<Meal> meals;
     
-    //Maps the "total_pages" property from JSON to this Java field
+    //Mapping of the "total_pages" JSON property to this field, storing total pages
     @JsonProperty("total_pages")
     private Integer totalPages;
     
-    //Maps the "total_results" property from JSON to this Java field
+    //Mapping of the "total_results" JSON property to this field, storing total results
     @JsonProperty("total_results")
     private Integer totalResults;
     
@@ -67,77 +66,77 @@ public class MealResult
     @JsonIgnore
     private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
     
-    //Getter for the "page" field, annotated to link with the JSON property
+    //Return the page mapped from the "page" JSON property
     @JsonProperty("page")
     public Integer getPage() 
     {
         return page;
     }
     
-    //Setter for the "page" field, annotated to link with the JSON property
+    //Set the page, mapping it to the "page" JSON property
     @JsonProperty("page")
     public void setPage(Integer page) 
     {
         this.page = page;
     }
     
-    //Getter for the "results" field (meals list), mapped from JSON "results"
+    //Return the meals mapped from the "meals" JSON property
     @JsonProperty("meals")
     public List<Meal> getMeals() 
     {
         return meals;
     }
     
-    //Setter for the "results" field (meals list), mapped from JSON "results"
+    //Set the meals, mapping it to the "meals" JSON property
     @JsonProperty("meals")
     public void setMeals(List<Meal> meals) 
     {
         this.meals = meals;
     }
     
-    //Getter for the "total_pages" field, linked to JSON property
+    //Return the total pages mapped from the "total_pages" JSON property
     @JsonProperty("total_pages")
     public Integer getTotalPages() 
     {
     return totalPages;
     }
     
-    //Setter for the "total_pages" field, linked to JSON property
+    //Set the total pages, mapping it to the "total_pages" JSON property
     @JsonProperty("total_pages")
     public void setTotalPages(Integer totalPages) 
     {
     this.totalPages = totalPages;
     }
     
-    //Getter for the "total_results" field, linked to JSON property
+    //Return the total results mapped from the "total_results" JSON property
     @JsonProperty("total_results")
     public Integer getTotalResults() 
     {
     return totalResults;
     }
     
-    //Setter for the "total_results" field, linked to JSON property
+    //Set the total results, mapping it to the "total_results" JSON property
     @JsonProperty("total_results")
     public void setTotalResults(Integer totalResults) 
     {
     this.totalResults = totalResults;
     }
     
-    //Getter for additional properties not explicitly mapped to a class field
+    /Return the map of additional properties not explicitly mapped in the class
     @JsonAnyGetter
     public Map<String, Object> getAdditionalProperties() 
     {
     return this.additionalProperties;
     }
     
-    //Setter for any additional JSON properties not mapped to a specific field
+    //Set a single additional property in the map
     @JsonAnySetter
     public void setAdditionalProperty(String name, Object value) 
     {
     this.additionalProperties.put(name, value);
     }
     
-    //Overrides the default toString() method to provide a human-readable representation of the object
+     //Formatting the display of meal from REST API to list on pages
     @Override
     public String toString() 
     {
