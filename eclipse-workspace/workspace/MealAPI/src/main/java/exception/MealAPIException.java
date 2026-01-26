d0e236dfa83c257e406353fed3c60e10e3fc33ab
alkpa1374 <105980104+alkpa1374@users.noreply.github.com>
2026-01-26 18:31:53 +0200

    Update MealAPIException.java

--- eclipse-workspace/workspace/MealAPI/src/main/java/exception/MealAPIException.java
+++ eclipse-workspace/workspace/MealAPI/src/main/java/exception/MealAPIException.java
@@ -1,28 +1,24 @@
-//Declare that this class belongs to the "exception" package
+//Class «MealAPIException» belongs package «exception»
 package exception;
 
-//Define a custom checked exception class named MealAPIException that extends the base Java Exception class
+//Class «MealAPIException» for handling exception during data reception from the REST API
 public class MealAPIException extends Exception
 {
-    //Define a unique serialVersionUID for serialization compatibility
-    //This ensures that the class can be serialized and deserialized consistently across JVMs
+    //Definition of a unique serialVersionUID for serialization compatibility, which this ensures that the class can be serialized and deserialized consistently across JVMs
     private static final long serialVersionUID = 5679558042286385889L;
     
-    //Constructor that takes only a message string as input
-    //Allows creating an exception with a descriptive message
+    //Constructor for creating an exception with a descriptive message
     public MealAPIException(String message) 
     {
-        //Call the parent Exception class constructor with the message
         super(message);
     }
     
-    //Constructor that takes a message string and a Throwable cause
-    //Allows creating an exception that wraps another exception
+    //Constructor for creating an exception with a descriptive message and a cause
     public MealAPIException(String message, Throwable cause) 
     {
-        //Call the parent Exception class constructor with the message and the cause
-        super(message, cause);
+       super(message, cause);
     }
 }
+
 
 
