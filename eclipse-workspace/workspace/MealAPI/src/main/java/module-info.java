725953e49cbb3287f10a2e56ee6ebae59ac5b168
alkpa1374 <105980104+alkpa1374@users.noreply.github.com>
2026-01-27 09:51:26 +0200

    Update module-info.java

--- eclipse-workspace/workspace/MealAPI/src/main/java/module-info.java
+++ eclipse-workspace/workspace/MealAPI/src/main/java/module-info.java
@@ -1,34 +1,34 @@
-//Declaration of module «gr.unipi.MealAPI»
+//Declaration of module "gr.unipi.MealAPI"
 module gr.unipi.MealAPI 
 {
 	
-    //Exportation of the package «model» so that other modules can use its public classes
+    //Exportation of the package "model" so that other modules can use its public classes
     exports model;
 
-    //Exportation of the package «model.themealdb» package for external access
+    //Exportation of the package "model.themealdb" for external access
     exports model.themealdb;
 
-    //Exportation of the package «service» package to provide API services to other modules
+    //Exportation of the package "service" to provide API services to other modules
     exports service;
 
-    //Exportation of the package «exception» package to allow external modules to handle custom exceptions
+    //Exportation of the package "exception" to allow external modules to handle custom exceptions
     exports exception;
 
-    //Requirements of Apache HttpClient for making HTTP requests to the Meal API
+    //Requirement of Apache HttpClient for making HTTP requests to the Meal API
     requires org.apache.httpcomponents.httpclient;
 
-    //Requirements of Apache HttpCore for low-level HTTP operations
+    //Requirement of Apache HttpCore for low-level HTTP operations
     requires org.apache.httpcomponents.httpcore;
 
-    //Requirements of Jackson Annotations for JSON serialization/deserialization annotations
+    //Requirement of Jackson Annotations for JSON serialization/deserialization annotations
     requires com.fasterxml.jackson.annotation;
 
-    //Requirement of Jackson Core for core JSON processing
+    //Requirement of Jackson Core for JSON processing
     requires com.fasterxml.jackson.core;
 
     //Requirement of Jackson Databind for converting JSON to Java objects and vice versa
     requires com.fasterxml.jackson.databind;
 
-    //Opening of the package «model.themealdb» specifically to Jackson Databind for reflection-based JSON processing
+    //Opening of the package "model.themealdb" specifically to Jackson Databind for reflection-based JSON processing
     opens model.themealdb to com.fasterxml.jackson.databind;
 }
