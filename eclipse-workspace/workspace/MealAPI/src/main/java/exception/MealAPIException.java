0783bf79f6447d58b99c8141dbf055c2cfedd4c7
alkpa1374 <105980104+alkpa1374@users.noreply.github.com>
2026-01-27 08:18:53 +0200

    Update MealAPIException.java

--- eclipse-workspace/workspace/MealAPI/src/main/java/exception/MealAPIException.java
+++ eclipse-workspace/workspace/MealAPI/src/main/java/exception/MealAPIException.java
@@ -1,10 +1,10 @@
-//Class «MealAPIException» belongs package «exception»
+//Class «MealAPIException» belongs to package «exception»
 package exception;
 
 //Class «MealAPIException» for handling exception during data reception from the REST API of THEMEALDB
 public class MealAPIException extends Exception
 {
-    //Definition of a unique serialVersionUID for serialization compatibility, which this ensures that the class can be serialized and deserialized consistently across JVMs
+    //Definition of a unique serialVersionUID for serialization compatibility, which ensures that the class can be serialized and deserialized consistently across JVMs
     private static final long serialVersionUID = 5679558042286385889L;
     
     //Constructor for creating an exception with a descriptive message
@@ -19,6 +19,7 @@ public class MealAPIException extends Exception
        super(message, cause);
     }
 }
+
 
 
 
