e2c1434554d812bb072f925200c1aa10ff991c5c
alkpa1374 <105980104+alkpa1374@users.noreply.github.com>
2026-01-28 08:45:01 +0200

    Update App.java

--- eclipse-workspace/workspace/MealFX/src/main/java/gr/unipi/MealFX/App.java
+++ eclipse-workspace/workspace/MealFX/src/main/java/gr/unipi/MealFX/App.java
@@ -1,4 +1,4 @@
-//Class «App» belongs package «gr.unipi.MealFX»
+//Class "App" belongs to package "gr.unipi.MealFX"
 package gr.unipi.MealFX;
 
 //Import of necessary JavaFX and utility classes
@@ -9,20 +9,20 @@ import javafx.scene.Scene;       //Representation of a scene in a Stage
 import javafx.stage.Stage;       //Representation of the top-level window
 import model.MealInfo;           //Import of the MealInfo model class
 
-//Class «App» for application implementation
+//Class "App" for application implementation
 public class App extends Application 
 {
-    //The primary stage (main window) of the application
+    //The application primary stage (main window)
     static Stage primaryStage;
 
     //Main Scene and Meal Scene
     static Scene mainScene, mealScene;
 
-    //Static lists to store favourite and cooked meals
+    //Static lists for favourite and cooked meals storage
     static List<MealInfo> FavMealList = new ArrayList<>();
     static List<MealInfo> CoMealList = new ArrayList<>();
 
-    //Override the start method called by JavaFX when the application launches
+    //Overriding of the start method called by JavaFX when overriding launched by the application
     @Override
     public void start(Stage Stage) 
     {
@@ -37,7 +37,7 @@ public class App extends Application
         MealSceneCreator mealSceneCreator = new MealSceneCreator();
         mealScene = mealSceneCreator.createScene();
 
-        //Set the title for primary stage
+        //Setting of the primary stage title
         primaryStage.setTitle("MainFX Window");
 
         //Initialization of main scene
@@ -47,10 +47,11 @@ public class App extends Application
         primaryStage.show();
     }
 
-    //Main method to launch the JavaFX application
+    //Main method for launching of the JavaFX application
     public static void main(String[] args) 
     {
         launch();
     }
 }
+
 
